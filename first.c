b752e19109030f44342e3efbc9cb81022ec73a7f
technicalblabla <30828707+technicalblabla@users.noreply.github.com>
2017-08-08 15:54:35 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a==0)
+{ printf("b if it's zero");
+}
+else if(a>0)
+{
+printf("if it's positive");
+}
+else if(a<0)
+{
+printf("if it's negative");
+}
+}
