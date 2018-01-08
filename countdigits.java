bfb5b6631ac095a36099aeb571b4abd1754080f5
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-08 11:10:48 +0000

    Create countdigits.java

---
+++ countdigits.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class len{
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=Integer.toString(a).length();
+		System.out.println(b);
+	}
+}
