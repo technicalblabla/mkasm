7a04b54fd24e019f5c8059c1eed02feb6004475c
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-19 10:52:53 +0000

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a, n, s1 = 0, s2 = 1, nt;
+
+    printf("Enter the number : ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (a = 1; a <= n; ++a)
+    {
+        printf("%d, ", s1);
+        nextTerm = s1 + s2;
+        s1 = s2;
+        s2 = nt;
+    }
+    return 0;
+}
