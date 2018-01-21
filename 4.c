f803dead479bc3697e91ad551e8021eeb1c47f2e
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-21 14:00:30 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the alphabet:");
+scanf("%c",&ch);
+if(ch=='a'||ch=='A')
+{
+printf("/n %c a is alphabet.",ch);
+}
+else
+{
+printf("/n %c a is not alphabet.",ch);
+}
+
+}
