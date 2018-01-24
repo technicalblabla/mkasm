9791359dd753999c0c4eb9e39f4906fe671ed9ee
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-24 16:38:52 +0530

    Rename 8.c to 08.c

---
+++ 08.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0;
+printf("\n enter the number");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("\n sum=%d",sum);
+getch();
+//print the values 
+}
