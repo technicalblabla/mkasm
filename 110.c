fee910afe006bae66ea2a4261431e879553ce6d7
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-24 16:31:43 +0530

    Create 110.c

---
+++ 110.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int  i,j,k;
+scanf("%d %d %d",&i,&j,&k);
+if(i>j)
+{
+printf("value %d is big",i);
+}
+elseif(j>k)
+{
+printf("value %d is big",j;
+}
+else
+{
+printf("value %d is big",k;
+}
+getch();
+}
