9fa132d576721a8d591764ab04eda0b5713d00a8
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-31 15:44:27 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,k;
+char a[20];
+printf("enter k");
+scanf("%d",&k);
+printf("enter the word");
+scanf("%s",a);
+for(i=0;i<=k;i++)
+{
+printf("%s",a);
+}
+}
