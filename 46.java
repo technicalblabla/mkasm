4ea753b03b5a20d0c1acb8b7b90468cc0fc9401d
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-31 15:57:33 +0530

    Create 46.java

---
+++ 46.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class mkasm_46
+{
+public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    int no= in.nextInt();
+    int b;
+    b=no+1;
+    System.out.println("next number is"+b);
+}}
