f94b3f3dd6625e4f08f0c8f4ba4b61c83a78cc1b
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-31 15:52:17 +0530

    Create 44.java

---
+++ 44.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class mkasm
+{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+if(x >=1 && x<=10)
+{
+	System.out.println("yes");
+}
+else
+{
+	System.out.println("no");
+}
+}
+}
