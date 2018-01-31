4dd3ebf3271443fba19a0191cbc0b6cf88961cb5
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-01-31 16:09:48 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char string[100];
+int i=0,l=0,f=1;
+clrscr();
+puts("Entr any string\n");
+gets(string);
+for(i=0;string[i]!='\0';i++)
+{
+l=l+1;
+}
+printf(" The number of lines in the string are %d\n",1);
+{
+if(sum[i]== ' ')
+{
+f=f+1;
+}
+}
