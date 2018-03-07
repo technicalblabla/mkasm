4ca24adcbf77c0545088976b4cce606cb311d869
Mohankumar <30828707+technicalblabla@users.noreply.github.com>
2018-03-07 16:07:43 +0530

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int arr[10],n,i,j,temp=0;
+    printf("Get The Input And Get The Range Of Array\n");
+    printf("Range:");scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+    {
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    
+        }
+    }
+        printf("The Smalest Value is:");
+        printf("%d",arr[0]);
+        
+}
